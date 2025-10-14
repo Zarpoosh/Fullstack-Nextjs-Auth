7f036829d6c75f69f0e8977d542a7851c68d80f4
Zarpoosh <mzar67839@gmail.com>
2025-10-14 19:32:21 +0330

    fix reset pass

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,6 +1,152 @@
+// "use client";
+// import React, { useEffect } from "react";
+// import { useState } from "react";
+// import {
+//   CardAction,
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card";
+// import { Label } from "@radix-ui/react-label";
+// import { Input } from "@/components/ui/input";
+// import { Button } from "@/components/ui/button";
+// import Link from "next/link";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import { resetPassword } from "@/lib/auth-client";
+
+// function ResetPasswordPage() {
+//   const [confirmpassword, setConfirmPassword] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState("");
+//   const [token, setToken] = useState<string | null>(null);
+//   const [loading, setLoading] = useState(false);
+//   const router = useRouter();
+//   const [success, setSuccess] = useState(false);
+
+//   const searchParams = useSearchParams();
+//   //   console.log(token);
+
+//   useEffect(() => {
+//     const tokenParam = searchParams.get("token");
+//     if (!tokenParam) {
+//       setError("invalid or missing reset token");
+//     } else {
+//       setToken(tokenParam);
+//     }
+//   }, [searchParams]);
+
+//   const handelSubmit = async (e: React.FormEvent) => {
+//     e.preventDefault();
+//     setError("");
+
+//     if (password !== confirmpassword) {
+//       setError("password do not match");
+//       return;
+//     }
+//     if (password.length < 8) {
+//       setError("password must be least 8 characters");
+//       return;
+//     }
+//     if (!token) {
+//       setError("invalid reset token");
+//       return;
+//     }
+
+//     setLoading(true);
+
+//     try {
+//       const result = await resetPassword({
+//         newPassword: password,
+//         token,
+//       });
+//       if (result.error) {
+//         setError(result.error.message || " Faield to reset the password");
+//       } else {
+//         setSuccess(true);
+//         setTimeout(() => {
+//           router.push("/login");
+//         }, 2000);
+//       }
+//     } catch (err) {
+//       setError(" An error occured please try again.");
+//       console.log("reset password error:", err);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   if (!token && !error) {
+//     return(
+//     <div className="min-h-screen flex items-center justify-center">
+//       <p>Loading ...</p>
+//     </div>
+//     )
+//   }
+
+//   return (
+//     <div className="flex min-h-screen items-center justify-center bg-background p-4">
+//       <Card className="w-full max-w-md">
+//         <CardHeader>
+//           <CardTitle>Reset Password</CardTitle>
+//           <CardDescription>Enter your new password</CardDescription>
+//           <CardAction>Card Action</CardAction>
+//         </CardHeader>
+//         <CardContent>
+//           <form className="space-y-4" onSubmit={handelSubmit}>
+//             <div>
+//               <Label htmlFor="password">new password</Label>
+//               <Input
+//                 id="password"
+//                 type="password"
+//                 placeholder="**********"
+//                 required
+//                 // value={Password}
+//                 onChange={(e) => setPassword(e.target.value)}
+//                 disabled={!token}
+//               />
+//             </div>
+//             <div>
+//               <Label htmlFor="confirmPassword">confirmPassword</Label>
+//               <Input
+//                 id="confirmPassword"
+//                 type="password"
+//                 placeholder="**********"
+//                 required
+//                 // value={confirmPassword}
+//                 onChange={(e) => setConfirmPassword(e.target.value)}
+//               />
+//             </div>
+//             {error && (
+//               <div className="rounded-md bg-destructive/10 p-3 text-sm text-destructive">
+//                 {error}
+//               </div>
+//             )}
+//             <Button
+//               className="w-full"
+//               //  disabled={loading}
+//               type="submit"
+//               disabled={loading || !token}
+//             >
+//               {loading ? "Reseting ...." : "Resset Password"}
+//             </Button>
+//           </form>
+//         </CardContent>
+//         <Button asChild variant="outline" className="w-full bg-transparent">
+//           <Link href="/login">Back to Login</Link>
+//         </Button>
+//       </Card>
+//     </div>
+//   );
+// }
+
+// export default ResetPasswordPage;
+
+
 "use client";
-import React, { useEffect } from "react";
-import { useState } from "react";
+
+import React, { useEffect, useState } from "react";
 import {
   CardAction,
   Card,
@@ -16,73 +162,66 @@ import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { resetPassword } from "@/lib/auth-client";
 
-function ResetPasswordPage() {
-  const [confirmpassword, setConfirmPassword] = useState("");
+export default function ResetPasswordPage() {
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [token, setToken] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
-  const router = useRouter();
   const [success, setSuccess] = useState(false);
-
+  const router = useRouter();
   const searchParams = useSearchParams();
-  //   console.log(token);
 
   useEffect(() => {
     const tokenParam = searchParams.get("token");
     if (!tokenParam) {
-      setError("invalid or missing reset token");
+      setError("Invalid or missing reset token");
     } else {
       setToken(tokenParam);
     }
   }, [searchParams]);
 
-  const handelSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setError("");
 
-    if (password !== confirmpassword) {
-      setError("password do not match");
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
       return;
     }
     if (password.length < 8) {
-      setError("password must be least 8 characters");
+      setError("Password must be at least 8 characters");
       return;
     }
     if (!token) {
-      setError("invalid reset token");
+      setError("Invalid reset token");
       return;
     }
 
     setLoading(true);
 
     try {
-      const result = await resetPassword({
-        newPassword: password,
-        token,
-      });
+      const result = await resetPassword({ newPassword: password, token });
       if (result.error) {
-        setError(result.error.message || " Faield to reset the password");
+        setError(result.error.message || "Failed to reset the password");
       } else {
         setSuccess(true);
-        setTimeout(() => {
-          router.push("/login");
-        }, 2000);
+        setTimeout(() => router.push("/login"), 2000);
       }
     } catch (err) {
-      setError(" An error occured please try again.");
-      console.log("reset password error:", err);
+      console.error("Reset password error:", err);
+      setError("An error occurred, please try again.");
     } finally {
       setLoading(false);
     }
   };
 
   if (!token && !error) {
-    return(
-    <div className="min-h-screen flex items-center justify-center">
-      <p>Loading ...</p>
-    </div>
-    )
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p>Loading ...</p>
+      </div>
+    );
   }
 
   return (
@@ -94,27 +233,25 @@ function ResetPasswordPage() {
           <CardAction>Card Action</CardAction>
         </CardHeader>
         <CardContent>
-          <form className="space-y-4" onSubmit={handelSubmit}>
+          <form className="space-y-4" onSubmit={handleSubmit}>
             <div>
-              <Label htmlFor="password">new password</Label>
+              <Label htmlFor="password">New Password</Label>
               <Input
                 id="password"
                 type="password"
-                placeholder="**********"
+                placeholder="********"
                 required
-                // value={Password}
                 onChange={(e) => setPassword(e.target.value)}
                 disabled={!token}
               />
             </div>
             <div>
-              <Label htmlFor="confirmPassword">confirmPassword</Label>
+              <Label htmlFor="confirmPassword">Confirm Password</Label>
               <Input
                 id="confirmPassword"
                 type="password"
-                placeholder="**********"
+                placeholder="********"
                 required
-                // value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
               />
             </div>
@@ -123,13 +260,8 @@ function ResetPasswordPage() {
                 {error}
               </div>
             )}
-            <Button
-              className="w-full"
-              //  disabled={loading}
-              type="submit"
-              disabled={loading || !token}
-            >
-              {loading ? "Reseting ...." : "Resset Password"}
+            <Button className="w-full" type="submit" disabled={loading || !token}>
+              {loading ? "Resetting ..." : "Reset Password"}
             </Button>
           </form>
         </CardContent>
@@ -140,5 +272,3 @@ function ResetPasswordPage() {
     </div>
   );
 }
-
-export default ResetPasswordPage;
